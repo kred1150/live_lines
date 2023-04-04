0c601191dfb62d2bd5f18df563fcd94891d52f4b
Kevin Redmond <kred1150@gmail.com>
2023-04-04 15:35:14 -0500

    Cleaned sports list to just the name of each sport, ie: 'MLB', or 'NCAAF'.

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,6 +2,10 @@ class SportsController < ApplicationController
   def index
     response = HTTP.get("https://api.the-odds-api.com/v4/sports?apiKey=#{Rails.application.credentials.odds_api_key}")
     sports = JSON.parse(response.body)
-    render json: sports
+    sport_names = []
+    sports.map do |sport|
+      sport_names << sport["title"]
+    end
+    render json: sport_names
   end
 end
