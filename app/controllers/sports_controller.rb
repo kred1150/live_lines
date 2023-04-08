ced0aa617b50e91fe4f80a958e66c94140f6406e
Kevin Redmond <kred1150@gmail.com>
2023-04-07 21:17:41 -0500

    Swtiched to the soccer only version with live scores for the main league. Added the API key to the credentials file, editied the controller file for soccer scores.

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,10 +2,6 @@ class SportsController < ApplicationController
   def index
     response = HTTP.get("https://api.the-odds-api.com/v4/sports?apiKey=#{Rails.application.credentials.odds_api_key}")
     sports = JSON.parse(response.body)
-    @sport_keys = []
-    sports.map do |sport|
-      @sport_keys << sport["key"]
-    end
     render json: sports
   end
 end
